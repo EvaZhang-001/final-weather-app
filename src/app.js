c347649e31e6735bf8678862051cb5cc14f46ffe
EvaZhang-001 <evazhang_001@hotmail.com>
2021-04-28 21:53:43 +0200

    updated temperature api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+function showTemperature(response) {
+  console.log(response);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
 
+let city = "New York";
+let apiKey = "68987c4f78ac703ea3b4c1f3b5c684ad";
+let unit = "metric";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+axios.get(url).then(showTemperature);
