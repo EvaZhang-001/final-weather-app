95900c6284418ac1995c71ca262d7dfb5e9bbef7
EvaZhang-001 <evazhang_001@hotmail.com>
2021-04-29 11:24:44 +0200

    updated day, date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
+function showTime(timestamp) {
+  let time = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[time.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[time.getMonth()];
+  let date = time.getDate();
+  let hour = time.getHours();
+  if (hour < 10) {
+    hour = `0:${hour}`;
+  }
+
+  let minute = time.getMinutes();
+  if (minute < 10) {
+    minute = `0:${minute}`;
+  }
+  return `${day}, ${month} ${date}, ${hour}:${minute}`;
+}
 function showTemperature(response) {
   console.log(response);
   let humidityElement = document.querySelector("#humidity");
@@ -6,9 +45,11 @@ function showTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = showTime(response.data.dt * 1000);
 }
 
-let city = "New York";
+let city = "Bern";
 let apiKey = "68987c4f78ac703ea3b4c1f3b5c684ad";
 let unit = "metric";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
