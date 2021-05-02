995ffb5242272006e213ac3a92d00a3d296ec960
EvaZhang-001 <evazhang_001@hotmail.com>
2021-05-02 15:33:28 +0200

    built search function

--- src/app.js
+++ src/app.js
@@ -38,7 +38,6 @@ function showTime(timestamp) {
   return `${day}, ${month} ${date}, ${hour}:${minute}`;
 }
 function showTemperature(response) {
-  console.log(response);
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
@@ -54,9 +53,21 @@ function showTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "bern";
-let apiKey = "68987c4f78ac703ea3b4c1f3b5c684ad";
-let unit = "metric";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 
-axios.get(url).then(showTemperature);
+function search(city) {
+  let apiKey = "68987c4f78ac703ea3b4c1f3b5c684ad";
+  let unit = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  axios.get(url).then(showTemperature);
+  let cityName = document.querySelector("#city");
+  cityName.innerHTML = city;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-input");
+  search(cityElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
